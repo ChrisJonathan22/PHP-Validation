4278ea2ef6ea538cbe55a2514b1e1b30983abbf6
Chris Jonathan <christophereko@hotmail.fr>
2019-04-06 04:48:53 +0100

    I've changed the homepage's ending path from index.html to index.php, I've also added some comments and deleted unnecessary code.

--- signin.php
+++ signin.php
@@ -8,20 +8,25 @@
 </head>
 <body>
     <?php 
+        // Once the form details are posted from index.php capture -
+        // the username and password.
         $userName = $_POST['username'];
         $password = $_POST['password'];
        
+        // Compare both the username and password and if there is a -
+        // match display the message below and display a link -
+        // back to the homepage.
         if(($userName == "admin") && ($password == "admin123")) {
             echo "<h1>Good to see you again $userName</h1>";
-            echo "<a href=\"http:localhost:3000/www/../../index.html\">Homepage</a>";
+            echo "<a href=\"http:localhost:3000/www/../../index.php\">Homepage</a>";
         }
 
+        // If the user's details do not match display the message below and -
+        // display a link back to the homepage.
         else {
             echo "<h1>Your account details do not match. Please try again.</h1>";
             echo "<br/>";
-            echo "<a href=\"http:localhost:3000/www/../../index.html\">Homepage</a>";
-            echo "<br/>";
-            echo "$list[0]";
+            echo "<a href=\"http:localhost:3000/www/../../index.php\">Homepage</a>";
         }
     ?>
 </body>
